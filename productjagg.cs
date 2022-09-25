392bb2cd2a808b4a142512a42b91f47ec365c900
AjayJosephRayen <113021792+AjayJosephRayen@users.noreply.github.com>
2022-09-25 21:29:07 +0530

    Add files via upload

---
+++ productjagg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maths
+{
+    internal class productjagg
+    {
+
+        public static void Main()
+        {
+            string[] list = new string[] { "A","B","C"};
+            string[] productname = new string[] { "Biscuits", "Chocolate", "Drinks"};
+            string[][] products = new string[3][];
+            products[0] = new string[] { "Oreo", "Bourbon", "Darkfantasy", "hide&seek" };
+            products[1] = new string[] { "Diary milk", "Twix", "hershey" };
+            products[2] = new string[] { "minutemaid", "fanta","pepsi","applejuice" };
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine("product" + i);
+                for (int j = 0; j < products[i].Length; j++)
+                {
+
+                    Console.Write(products[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
