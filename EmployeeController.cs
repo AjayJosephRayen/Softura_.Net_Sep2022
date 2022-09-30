1e09c28e2996573fd0bfed6021dd643014b682be
AjayJosephRayen <113021792+AjayJosephRayen@users.noreply.github.com>
2022-09-30 09:05:52 +0530

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC2.Models;
+
+namespace MVC2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Emp()
+        {
+            return "Emplyoee Credentials";
+        }
+        public IActionResult Employeedetails()
+        {
+            empdetails details = new empdetails()
+            { Id = 100, Name = "Ajay", Department = "EEE" };
+            return View("empinfo", details);
+        
+        }
+    }
+}
