b6fcee7b9dbf742189106c4c39c30945c8e30766
AjayJosephRayen <113021792+AjayJosephRayen@users.noreply.github.com>
2022-10-03 18:30:59 +0530

    Add files via upload

---
+++ task12/Controllers/EmpController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeTsk.Models;
+
+namespace EmployeeTsk.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult singleemp()
+        { 
+        Employee emp1=new Employee();
+            emp1 = emp1.getemp();
+            return View(emp1);
+        
+        }
+        public IActionResult allemployees()
+        { 
+        List<Employee> emp2=Employee.allemp();
+            return View(emp2);
+        }
+        public IActionResult AddDatas()
+        {
+            return View();
+        }
+    }
+}
---
+++ task12/Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿namespace EmployeeTsk.Models
+{
+    public class Employee
+    {
+       public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public char Gender { get; set; }
+        public  Employee getemp()
+        {
+            Employee employee = new Employee()
+            {
+                Id = 001,
+                Firstname = "Ajay",
+                Lastname = "Joseph Rayen",
+                Gender = 'M'
+
+
+
+            };
+            return employee;
+
+        
+        }
+        public static List<Employee> allemp()
+        {
+            List<Employee> employees1 = new List<Employee>()
+            {
+                new Employee() { Id = 1001, Firstname = "Anika", Lastname = "Rhesa Rayen", Gender = 'F' },
+                 new Employee() { Id = 1002, Firstname = "Trisha", Lastname = " Rayen", Gender = 'F' },
+                  new Employee() { Id = 1003, Firstname = "Murali", Lastname = "Krishna", Gender = 'M' },
+                  new Employee() { Id = 1004, Firstname = "Cyril", Lastname = "Poul Rayen", Gender = 'M' },
+                  new Employee() { Id = 1005, Firstname = "Lakshmi", Lastname = "Kumari", Gender = 'F' }
+            };
+            return employees1;
+        }
+    }
+}
